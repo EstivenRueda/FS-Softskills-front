d8ae3d3b67acaa94ad2742332a88779bc2487e85
Carlos-Aucaruri <carlos.aucaruri@gmail.com>
2025-01-25 17:16:26 -0500

    fixed env vars in google constants

--- src/app/(auth)/constants/google.ts
+++ src/app/(auth)/constants/google.ts
@@ -1,3 +1,3 @@
-const { BASE_APP_URL, GOOGLE_OAUTH_CLIENT_ID } = process.env;
-export const googleCallbackUri = `${BASE_APP_URL}/google/callback`;
-export const googleClientId = GOOGLE_OAUTH_CLIENT_ID;
+const { NEXT_PUBLIC_BASE_APP_URL, NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID } = process.env;
+export const googleCallbackUri = `${NEXT_PUBLIC_BASE_APP_URL}/google/callback`;
+export const googleClientId = NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID;
